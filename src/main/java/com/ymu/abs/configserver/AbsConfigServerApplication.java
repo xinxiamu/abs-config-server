dbe3617e57fab3f280eb27133e47d20e586161da
xinxiamu <932852117@qq.com>
2018-01-24 20:36:37 +0800

    基础配置调整

--- src/main/java/com/ymu/abs/configserver/AbsConfigServerApplication.java
+++ src/main/java/com/ymu/abs/configserver/AbsConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.ymu.abs.absconfigserver;
+package com.ymu.abs.configserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/ymu/abs/configserver/AbsConfigServerApplicationTests.java
+++ src/test/java/com/ymu/abs/configserver/AbsConfigServerApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ymu.abs.absconfigserver;
+package com.ymu.abs.configserver;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
